468018b59abd5bb7b55bb2d1e5f1221bdc9a618a
Eric Gilbertson <Gilbertson_Eric@bah.com>
2026-02-05 08:03:00 -0700

    file download controls finalized

--- app/src/download_and_sort_data.py
+++ app/src/download_and_sort_data.py
@@ -1,21 +1,23 @@
 import os
 import json
+import pickle
 import datetime
 import pandas as pd
+from NBASeason import NBASeason
 
-# from tempfile import TemporaryDirectory
+# from constants import MAXSIZE
+from sys import maxsize as MAXSIZE
+from functions import DATA_DATE_FORMAT_STRING, DATA_TIME_FORMAT_STRING
 
 with open("app/data/config.json", "r") as file:
     config = json.load(file)
 with open("app\\data\\name_to_id.json", "r") as file:
     ids = json.load(file)
 with open(config["KAGGLE_API_TOKEN_PATH"], "r") as file:
-    KAGGLE_API_TOKEN = file.read()
-os.environ["KAGGLE_API_TOKEN"] = KAGGLE_API_TOKEN
+    os.environ["KAGGLE_API_TOKEN"] = file.read()
 
 import kaggle
 
-# DATA_DATE_FORMAT_STRING = "%Y-%m-%d %H:%M:%S"
 COLUMNS_TO_KEEP = [
     "gameDateTimeEst",
     "hometeamCity",
@@ -29,52 +31,79 @@ COLUMNS_TO_KEEP = [
     "winner",
     "gameType",
     # "seriesGameNumber",
-    # TODO do playoffs include these stats, or can we predict something else from this?
+    # TODO do playoffs influence the stats/models? Do we treat them as a separate data set? Do we include them in the data set?
 ]
 SEASON_PATH = os.path.join(config["DATA_DOWNLOAD_PATH"], "seasons")
 DOWNLOAD_TIME_FILEPATH = os.path.join(config["DATA_DOWNLOAD_PATH"], "last_download")
+LAST_DOWNLOAD_TIME_FORMAT_STRING = DATA_DATE_FORMAT_STRING + " " + DATA_TIME_FORMAT_STRING
 
 
-def download_csv(path=config["DATA_DOWNLOAD_PATH"], force=True, quiet=False):
-    kaggle.api.dataset_download_file(dataset=config["DATASET_NAME"], file_name=config["DATA_FILE_NAME"], path=path, force=force, quiet=quiet)
-    # TODO put the full games.csv into only a temporary folder, then sort and delete the raw data post sorting
+def download_csv(path=config["DATA_DOWNLOAD_PATH"], quiet=False):
+    kaggle.api.dataset_download_file(dataset=config["DATASET_NAME"], file_name=config["DATA_FILE_NAME"], path=path, force=True, quiet=quiet)
     with open(DOWNLOAD_TIME_FILEPATH, "w") as file:
-        file.write(str(datetime.datetime.now(datetime.timezone.utc).isoformat()))
+        file.write(datetime.datetime.now(datetime.timezone.utc).isoformat())
 
 
-def sort_data_by_season(df, path=SEASON_PATH):
+def sort_data_by_season(df: pd.DataFrame, path=SEASON_PATH, full=False):
     # loop throught raw dataframe, save raw data to a folder named after the year(s) in question
-    # assumes the first day of the season occurs after August 1st of that year, and concludes before August 1st the following year
     os.makedirs(path, exist_ok=True)
-    for i in range(config["MIN_SEASON_YEAR"], datetime.date.today().year + 1):
-        season_df = df[(df["gameDateTimeEst"] > f"{i}-08-01 00:00:00") & (df["gameDateTimeEst"] < f"{i+1}-08-01 00:00:00")]
+    for year in range(config["MIN_SEASON_YEAR"] if full else datetime.date.today().year - 2, datetime.date.today().year + 1):
+        # assumes the first day of the season occurs after August 1st of that year, and concludes before August 1st the following year, split to avoid dataframe lock
+        season_df = df[(df["gameDateTimeEst"] > f"{year}-08-01 00:00:00") & (df["gameDateTimeEst"] < f"{year+1}-08-01 00:00:00")]
         if not season_df.empty:
-            dir_path = os.path.join(path, f"{i}-{i+1}")
-            file_path = os.path.join(dir_path, f"{i}-{i+1}_raw.csv")
+            # helper nums
+            dir_path = os.path.join(path, f"{year}-{year+1}")
+            raw_file_path = os.path.join(dir_path, f"{year}-{year+1}_raw.csv")
+            full_file_path = os.path.join(dir_path, f"{year}-{year+1}_full.csv")
+            class_file_path = os.path.join(dir_path, f"{year}-{year+1}_season.pkl")
+            season = NBASeason(season_df)
+            full_df = season.generate_and_save_full_season_data()
+
+            # save data
             os.makedirs(dir_path, exist_ok=True)
-            season_df.to_csv(file_path, index=False)
-            print(f"Saved {i}-{i+1} game data to {file_path}")
+            season_df.to_csv(raw_file_path, index=False)
+            full_df.to_csv(full_file_path, index=False)
+            with open(class_file_path, "wb") as file:
+                pickle.dump(season, file, pickle.HIGHEST_PROTOCOL)
+            print(f"Saved {year}-{year+1} game data to {dir_path}")
 
 
 ### MAIN FUNCTION/PROCESS FOR THIS FILE
 def download_and_sort_data():
     # determine if we should request kaggle and get a new set of data
+    if os.path.isfile(DOWNLOAD_TIME_FILEPATH):
+        with open(DOWNLOAD_TIME_FILEPATH) as file:
+            last_download_time = datetime.datetime.fromisoformat(file.read())
+            download_time_delta_hrs = (datetime.datetime.now(datetime.timezone.utc) - last_download_time).seconds // 3600
+    else:
+        download_time_delta_hrs = MAXSIZE
+    skip_download = download_time_delta_hrs < config["MAX_FILE_AGE_HRS"] and os.path.isdir(config["DATA_DOWNLOAD_PATH"])
+    full_download = config["FORCE_FILE_UPDATE"] or not os.path.isdir(config["DATA_DOWNLOAD_PATH"])
+    print("SKIP?          ", skip_download)
+    print("FULL?          ", full_download)
+    print("HRS SINCE LAST:", download_time_delta_hrs)
+
+    # download and sorting logic
     os.makedirs(config["DATA_DOWNLOAD_PATH"], exist_ok=True)
-    if os.path.isfile(DOWNLOAD_TIME_FILEPATH) and not config["FORCE_FILE_UPDATE"]:
-        print(f"Using previously downloaded {config['DATA_FILE_NAME']} from XX:XX:XXZ")  # TODO say when you last downloaded games.csv
+    if skip_download and not full_download:
+        print(
+            f"Using previously downloaded {config['DATA_FILE_NAME']} from {last_download_time.astimezone().strftime(LAST_DOWNLOAD_TIME_FORMAT_STRING)}"
+        )
+        return
     else:
         download_csv()
         print(f"Downloaded {config['DATA_FILE_NAME']}")
-    # TODO if the last download time is older than a certain number of hours or we force it to update, update
-    # and if force update, remove everything inside the config data download path
 
-    # trim data to only needed columns, sort by season year(s)
-    print("Sorting game data by season")
-    raw_df = pd.read_csv(os.path.join(config["DATA_DOWNLOAD_PATH"], config["DATA_FILE_NAME"]), low_memory=False)[COLUMNS_TO_KEEP]
-    raw_df = raw_df[(raw_df["gameType"] != "Preseason") & (raw_df["gameType"] != "Play-in Tournament")]
-    id_list = list(ids.values())
-    raw_df = raw_df.query("(hometeamId in @id_list) & (awayteamId in @id_list)")
-    sort_data_by_season(raw_df)
-    # TODO ^^^expand sorting logic to eliminate teams and games that don't count toward standings, particularly NBA Cup Finals and Global Games
-
-    # thread each season folder, do the rest of preprocessing in parallel, then wait and return
+        # trim data to only needed columns, season sort by year
+        print("Sorting game data by season")
+        id_list = list(ids.values())  # TODO combine abbreviations and ids into one lookup json
+        raw_df = pd.read_csv(os.path.join(config["DATA_DOWNLOAD_PATH"], config["DATA_FILE_NAME"]), low_memory=False)[COLUMNS_TO_KEEP]
+        raw_df = raw_df[
+            (raw_df["homeScore"] != 0)
+            & (raw_df["awayScore"] != 0)
+            & (raw_df["gameType"] != "Preseason")
+            & (raw_df["gameType"] != "Play-in Tournament")
+            & (raw_df["gameType"] != "Playoffs")  # TODO configify playoffs exclusion
+        ]
+        raw_df = raw_df.query("(hometeamId in @id_list) & (awayteamId in @id_list)")
+        sort_data_by_season(raw_df, full=full_download)
--- app/src/main.py
+++ app/src/main.py
@@ -1,5 +1,19 @@
 from download_and_sort_data import download_and_sort_data
 
+# DEBUG IMPORTS
+import pickle
+import pandas as pd
+
+# TODO delete debug imports
+
 if __name__ == "__main__":
     print("----- WELCOME TO THE BASKETBALL COMPUTER THINGY -----")
     download_and_sort_data()
+
+    # debug
+    test_year = "2025-2026"  # "1946-1947" "1995-1996" "2025-2026"
+    with open(f"app\\data\\games\\seasons\\{test_year}\\{test_year}_season.pkl", "rb") as file:
+        test_season = pickle.load(file)
+    test_season_df = pd.read_csv(f"app\\data\\games\\seasons\\{test_year}\\{test_year}_full.csv")
+    print(test_season_df)
+    test_season.pretty_print()
