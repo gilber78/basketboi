15429ca5e3c43171df22c6445ef5c965ad36f32b
Eric Gilbertson <Gilbertson_Eric@bah.com>
2026-02-17 06:40:14 -0700

    configified playoff inclusion in reference data. added logic to do a full reset after a certain number of days (currently set to 30)

--- app/src/download_and_sort_data.py
+++ app/src/download_and_sort_data.py
@@ -26,7 +26,7 @@ COLUMNS_TO_KEEP = [
     "winner",
     "gameType",
     # "seriesGameNumber",
-    # TODO do playoffs influence the stats/models? Do we treat them as a separate data set? Do we include them in the data set?
+    # TODO does playoff series length influence the stats/models? Do we treat them as a separate data set? Do we include them in the data set?
 ]
 
 
@@ -36,10 +36,13 @@ def download_csv(path, file_name, download_time_filepath, dataset, quiet=False):
         file.write(datetime.datetime.now(datetime.timezone.utc).isoformat())
 
 
-def sort_data_by_season(df: pd.DataFrame, path, min_season_year, full=False):
+def sort_data_by_season(df: pd.DataFrame, path, min_season_year, reset_time_filepath, full=False):
     # loop throught raw dataframe, save raw data to a folder named after the year(s) in question
     os.makedirs(path, exist_ok=True)
     current_season_year = get_current_season_year()
+    if full:
+        with open(reset_time_filepath, "w") as file:
+            file.write(datetime.datetime.now(datetime.timezone.utc).isoformat())
     for year in range(min_season_year if full else current_season_year, current_season_year + 1):
         # assumes the first day of the season occurs after August 1st of that year, and concludes before August 1st the following year, split to avoid dataframe lock
         season_df = df[(df["gameDateTimeEst"] > f"{year}-08-01 00:00:00") & (df["gameDateTimeEst"] < f"{year+1}-08-01 00:00:00")]
@@ -65,6 +68,7 @@ def sort_data_by_season(df: pd.DataFrame, path, min_season_year, full=False):
 def download_and_sort_data(config):
     # read from config
     DOWNLOAD_TIME_FILEPATH = os.path.join(config["DATA_DOWNLOAD_PATH"], "last_download")
+    RESET_TIME_FILEPATH = os.path.join(config["DATA_DOWNLOAD_PATH"], "last_reset")
     LAST_DOWNLOAD_TIME_FORMAT_STRING = DATA_DATE_FORMAT_STRING + " " + DATA_TIME_FORMAT_STRING
 
     # determine if we should request kaggle and get a new set of data
@@ -74,9 +78,16 @@ def download_and_sort_data(config):
             download_time_delta_hrs = (datetime.datetime.now(datetime.timezone.utc) - last_download_time).total_seconds() // 3600
     else:
         download_time_delta_hrs = MAXSIZE
+    if os.path.isfile(RESET_TIME_FILEPATH):
+        with open(RESET_TIME_FILEPATH) as file:
+            last_reset_time = datetime.datetime.fromisoformat(file.read())
+            reset_time_delta_days = (datetime.datetime.now(datetime.timezone.utc) - last_reset_time).total_seconds() // 86400
+    else:
+        reset_time_delta_days = MAXSIZE
     skip_download = download_time_delta_hrs < config["MAX_FILE_AGE_HRS"] and os.path.isdir(config["DATA_DOWNLOAD_PATH"])
-    full_download = config["FORCE_FILE_UPDATE"] or not os.path.isdir(config["DATA_DOWNLOAD_PATH"])
-    # TODO add that if it's been a REALLY long time that we force an update
+    full_download = (
+        reset_time_delta_days >= config["MAX_RESET_AGE_DAYS"] or config["FORCE_FILE_UPDATE"] or not os.path.isdir(config["DATA_DOWNLOAD_PATH"])
+    )
 
     # download and sorting logic
     os.makedirs(config["DATA_DOWNLOAD_PATH"], exist_ok=True)
@@ -98,16 +109,18 @@ def download_and_sort_data(config):
         print("Sorting game data by season")
         id_list = list(ids.values())  # TODO combine abbreviations and ids into one lookup json
         raw_df = pd.read_csv(os.path.join(config["DATA_DOWNLOAD_PATH"], config["DATA_FILE_NAME"]), low_memory=False)[COLUMNS_TO_KEEP]
-        raw_df = raw_df[
-            (raw_df["homeScore"] != 0)
-            & (raw_df["awayScore"] != 0)
-            & (raw_df["gameType"] != "Preseason")
-            & (raw_df["gameType"] != "Play-in Tournament")
-            & (raw_df["gameType"] != "Playoffs")  # TODO configify playoffs exclusion
-        ]
+        raw_df = raw_df[(raw_df["homeScore"] != 0) & (raw_df["awayScore"] != 0) & (raw_df["gameType"] != "Preseason")]
+        if not config["INCLUDE_PLAYOFFS"]:
+            raw_df = raw_df[(raw_df["gameType"] != "Play-in Tournament") & (raw_df["gameType"] != "Playoffs")]
         raw_df = raw_df.query("(hometeamId in @id_list) & (awayteamId in @id_list)")
         if full_download:
             print("Performing full data update")
         else:
             print("Updating current season only")
-        sort_data_by_season(raw_df, path=os.environ["SEASON_PATH"], min_season_year=config["MIN_SEASON_YEAR"], full=full_download)
+        sort_data_by_season(
+            raw_df,
+            path=os.environ["SEASON_PATH"],
+            min_season_year=config["MIN_SEASON_YEAR"],
+            reset_time_filepath=RESET_TIME_FILEPATH,
+            full=full_download,
+        )
